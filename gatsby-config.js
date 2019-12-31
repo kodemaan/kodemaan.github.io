f75a1895b2609a8e67aced97a152bd0bf183934e
Steven Smith <kodemaan@gmail.com>
2019-12-31 13:58:19 -0500

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,14 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitle: `Steven Smith - Senior Software Engineer`
+    siteTitle: `Steven Smith - Senior Software Engineer`,
+    siteTitleAlt: `Steven Smith - Senior Software Engineer`,
+    siteHeadline: `Steven Smith - Senior Software Engineer`,
+    siteUrl: 'https://kodemaan.github.io',
+    siteDescription: 'Personal website for Steven Smith',
+    siteLanguage: 'en',
+    siteImage: '/banner.jpg',
+    author: '@kodemaan'
   },
   plugins: [
     {
