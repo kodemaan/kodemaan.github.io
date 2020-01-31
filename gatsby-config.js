0b47f5d443a1fcdb67d0675a1e2a46bfcc099701
Steven Smith <kodemaan@gmail.com>
2020-01-31 08:19:32 -0500

    Updating gatsby version and updating the yarn lock

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,44 +7,44 @@ module.exports = {
     siteTitle: `Steven Smith - Senior Software Engineer`,
     siteTitleAlt: `Steven Smith - Senior Software Engineer`,
     siteHeadline: `Steven Smith - Senior Software Engineer`,
-    siteUrl: 'https://kodemaan.github.io',
+    siteUrl: 'https://kodemaan-personal-site.netlify.com',
     siteDescription: 'Personal website for Steven Smith',
     siteLanguage: 'en',
-    siteImage: '/banner.jpg',
-    author: '@kodemaan',
-    externalLinks: [
-      {
-        name: `Twitter`,
-        url: `https://www.twitter.com/kodemaan`
-      },
-      {
-        name: `Github`,
-        url: `https://www.github.com/kodemaan`
-      },
-      {
-        name: `Linkedin`,
-        url: `https://linkedin.com/in/steven-smith-b4b5a41b`
-      }
-    ],
-    navigation: [
-      {
-        title: `Contact`,
-        slug: `/contact`
-      },
-      {
-        title: `Projects`,
-        slug: `/projects`
-      },
-      {
-        title: `Resume`,
-        slug: `/resume`
-      }
-    ]
+    author: '@kodemaan'
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-      options: {},
+      options: {
+        navigation: [
+          {
+            title: `Contact`,
+            slug: `/contact`
+          },
+          {
+            title: `Projects`,
+            slug: `/projects`
+          },
+          {
+            title: `Resume`,
+            slug: `/resume`
+          }
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://www.twitter.com/kodemaan`
+          },
+          {
+            name: `Github`,
+            url: `https://www.github.com/kodemaan`
+          },
+          {
+            name: `Linkedin`,
+            url: `https://linkedin.com/in/steven-smith-b4b5a41b`
+          }
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
